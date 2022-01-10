4de1dfb808015aa74482826d904318dc1e61663e
tung3122002 <tung3122002@gmail.com>
2022-01-10 10:57:18 +0700

    fix menu

--- src/main.js
+++ src/main.js
@@ -7,25 +7,28 @@
 //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 // `;
 import Navigo from "navigo";
-import menu from "./data";
+// import menu from "./data";
 
-const showmenu = () => {
-    const menuElement = document.querySelector("#menu1");
-    if (menuElement) {
-        const kq = menu.map((item) => `
-        <li class="leading-[50px] hover:bg-[#272f54]  hover:text-white mx-3"><a class="  hover:text-white"
-        href="">${item}</a></li>
-        
-        `).join("");
-        menuElement.innerHTML = kq;
-    }
-};
-showmenu();
+// const showmenu = () => {
+//     const menuElement = document.querySelector("#menu1");
+//     if (menuElement) {
+//         const kq = menu.map((item) => `
+//         <li class="leading-[50px] hover:bg-[#272f54]  hover:text-white mx-3"><a class="  hover:text-white"
+//         href="">${item}</a></li>
+
+//         `).join("");
+//         menuElement.innerHTML = kq;
+//     }
+// };
+// showmenu();
 
 const router = new Navigo("/", { linksSelector: "a" });
 router.on({
-    // nếu  user truy cập vào đường dẫn  thì sẽ làm j đó
+    // Nếu user truy cập vào đường dẫn / thì sẽ làm việc gì đó
     "/": () => console.log("home page"),
-    "/about": () => console.log("about page"),
+    "/about": () => console.log("About page"),
+    "/product": () => console.log("Product Page"),
 });
+router.notFound(() => console.log("not found page"));
+
 router.resolve();
