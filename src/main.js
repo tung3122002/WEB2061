21f3c88a971ef5678d815e78ab14d83add6dba3e
tung3122002 <tung3122002@gmail.com>
2022-01-10 11:05:52 +0700

    fix: menu

--- src/main.js
+++ src/main.js
@@ -7,20 +7,20 @@
 //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 // `;
 import Navigo from "navigo";
-// import menu from "./data";
+import menu from "./data";
 
-// const showmenu = () => {
-//     const menuElement = document.querySelector("#menu1");
-//     if (menuElement) {
-//         const kq = menu.map((item) => `
-//         <li class="leading-[50px] hover:bg-[#272f54]  hover:text-white mx-3"><a class="  hover:text-white"
-//         href="">${item}</a></li>
-
-//         `).join("");
-//         menuElement.innerHTML = kq;
-//     }
-// };
-// showmenu();
+const showmenu = () => {
+    const menuElement = document.querySelector("#menu1");
+    if (menuElement) {
+        const kq = menu.map((item) => `
+        <li class="leading-[50px] hover:bg-[#272f54]  hover:text-white mx-3"><a class="  hover:text-white"
+        href="">${item}</a></li>
+        
+        `).join("");
+        menuElement.innerHTML = kq;
+    }
+};
+showmenu();
 
 const router = new Navigo("/", { linksSelector: "a" });
 router.on({
