e82a22f021ad76289416f7a63d4569c067229837
tung3122002 <tung3122002@gmail.com>
2022-01-15 08:09:28 +0700

    fix lôi " ,,"

--- src/components/new.js
+++ src/components/new.js
@@ -11,11 +11,11 @@ const News = {
         ${news.map((item) => `
         <div class="box-tintuc  w-[312px] h-[359px] border-2 p-[25px] border-rose-600 ">
         <div class="img-tintuc max-w-[310px] truncate ">
-        <a href="/product/${item.id}"><p><img class="hover:scale-110 truncate transition ease-in-out delay-75" src="${item.img}" alt="">”</p></a>
+        <a href="/product/${item.id}"><p><img class="hover:scale-110 truncate transition ease-in-out delay-75" src="${item.img}" alt=""></p></a>
           
         </div>
         <div class="tintuc-text text-[#ad3f00] font-bold my-[10px] hover:text-[#00467F]">
-        <a href="/product/${item.id}"><p>${item.title}”</p></a>
+        <a href="/product/${item.id}"><p>${item.title}</p></a>
         </div>
         <div class="tintuc-text2 text-[12px] hover:text-[#00467F]">
           <p>${item.desc}</p>
--- src/page/detall.js
+++ src/page/detall.js
@@ -6,10 +6,11 @@ const DetailPage = {
         const result = data.find((post) => post.id === id);
 
         return `
+        
         ${Header.render()}
         <div class="box-tintuc  w-[312px] h-[359px] border-2 p-[25px] border-rose-600 my-12">
         <div class="img-tintuc max-w-[310px] truncate ">
-          <img class="hover:scale-110 truncate transition ease-in-out delay-75" src="${result.img}" alt="">
+          <img class="hover:scale-110 truncate transition ease-in-out delay-75" src="${result.img}"></ alt="">
         </div>
         <div class="tintuc-text text-[#ad3f00] font-bold my-[10px] hover:text-[#00467F]">
         <a href="/product/${result.id}"><p>${result.title}</p></a>
